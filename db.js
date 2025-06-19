1ccd7e53a5cf22fd21ac529b01fd9f4c8a67014e
Vishal <vishalkumar.ps@outlook.com>
2025-06-19 15:50:43 +0530

    login issue fixed

--- db.js
+++ db.js
@@ -1,13 +1,11 @@
-// db.js
 const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Needed for Render PostgreSQL
+  },
 });
 
 module.exports = pool;
